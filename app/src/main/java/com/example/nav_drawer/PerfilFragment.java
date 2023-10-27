20b8014e72fe7b38ae252f692894a4cce5d156ab
Javier <fflores22@ucol.mx>
2023-10-27 13:32:19 -0600

    Comente perfil

--- app/src/main/java/com/example/nav_drawer/PerfilFragment.java
+++ app/src/main/java/com/example/nav_drawer/PerfilFragment.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+//PERFIL
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PerfilFragment#newInstance} factory method to
