d7ef6645060c245385f39497bb2f7d7311e681eb
Javier <fflores22@ucol.mx>
2023-11-20 03:00:42 -0600

    Crear tabla de chatroom de doctores funcional

--- app/src/main/java/com/example/nav_drawer/viewpaciente/Activity_Chat_Paciente.java
+++ app/src/main/java/com/example/nav_drawer/viewpaciente/Activity_Chat_Paciente.java
@@ -1,19 +1,26 @@
 package com.example.nav_drawer.viewpaciente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.nav_drawer.AndroidUtilDoctor;
+import com.example.nav_drawer.ChatMessageModel;
 import com.example.nav_drawer.ChatroomModelDoctor;
 import com.example.nav_drawer.DoctorModel;
 import com.example.nav_drawer.FirebaseUtilDoctor;
 import com.example.nav_drawer.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.Arrays;
 
@@ -35,11 +42,11 @@ public class Activity_Chat_Paciente extends AppCompatActivity {
         otherDoctor = AndroidUtilDoctor.getDoctorModelFromIntent(getIntent());
         chatroomId = FirebaseUtilDoctor.getChatroomId(FirebaseUtilDoctor.currentDoctorId(),otherDoctor.getId());
 
-        messageInput = findViewById(R.id.chat_message_input);
-        sendMessageBtn = findViewById(R.id.message_send_btn);
-        backBtn = findViewById(R.id.back_btn);
-        otherDoctorname = findViewById(R.id.other_username);
-        recyclerView = findViewById(R.id.chat_recycler_view);
+        messageInput = findViewById(R.id.chat_message_inputs);
+        sendMessageBtn = findViewById(R.id.message_send_btns);
+        backBtn = findViewById(R.id.back_btns);
+        otherDoctorname = findViewById(R.id.other_usernames);
+        recyclerView = findViewById(R.id.chat_recycler_views);
 
         backBtn.setOnClickListener((v)->{
             onBackPressed();
@@ -47,24 +54,64 @@ public class Activity_Chat_Paciente extends AppCompatActivity {
 
         otherDoctorname.setText("Medico "+otherDoctor.getNombre());
 
+
+        sendMessageBtn.setOnClickListener(v -> {
+            String message = messageInput.getText().toString().trim();
+            if (message.isEmpty()) {
+                return;
+            }
+            sendMessageToDoctor(message);
+        });
+
         getOrCreateChatroomModelDoctor();
 
     }
 
-    void getOrCreateChatroomModelDoctor(){
+    void getOrCreateChatroomModelDoctor() {
         FirebaseUtilDoctor.getChatroomReference(chatroomId).get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()){
-                chatroomModelDoctor = task.getResult().toObject(chatroomModelDoctor.getClass());
-                if(chatroomModelDoctor==null){
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document != null && document.exists()) {
+                    chatroomModelDoctor = document.toObject(ChatroomModelDoctor.class);
+                } else {
                     chatroomModelDoctor = new ChatroomModelDoctor(
                             chatroomId,
-                            Arrays.asList(FirebaseUtilDoctor.currentDoctorId(),otherDoctor.getId()),
+                            Arrays.asList(FirebaseUtilDoctor.currentDoctorId(), otherDoctor.getId()),
                             Timestamp.now(),
                             ""
                     );
                     FirebaseUtilDoctor.getChatroomReference(chatroomId).set(chatroomModelDoctor);
                 }
+            } else {
+                // Manejar la excepción si la operación no fue exitosa
+                Exception exception = task.getException();
+                if (exception != null) {
+                    // Mostrar la traza de la excepción
+                    exception.printStackTrace();
+                }
             }
         });
     }
+
+
+    void sendMessageToDoctor(String message){
+
+        chatroomModelDoctor.setLastMessageTimestamp(Timestamp.now());
+        chatroomModelDoctor.setLastMessageSenderId(FirebaseUtilDoctor.currentDoctorId());
+
+        FirebaseUtilDoctor.getChatroomReference(chatroomId).set(chatroomModelDoctor);
+
+        ChatMessageModel chatMessageModel = new ChatMessageModel(message,FirebaseUtilDoctor.currentDoctorId(),Timestamp.now());
+        FirebaseUtilDoctor.getChatroomMessageReference(chatroomId).add(chatMessageModel)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if(task.isSuccessful()){
+                            messageInput.setText("");
+                        }
+                    }
+                });
+    }
+
+
 }
